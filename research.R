1907ead3c11dd862f4928d5da19975ad45b50d39
Daniel Wang <s92025592025@gmail.com>
2017-02-28 22:43:45 -0800

    Add plot for attack per year

--- research.R
+++ research.R
@@ -2,7 +2,11 @@ library(dplyr)
 library(plotly)
 library(ggplot2)
 
+source('./API.R')
 DATA <- read.csv('./data/globalterrorismdb_0616dist.csv', stringsAsFactors = TRUE)
+Sys.setenv("plotly_username" = "s92025592025")
+Sys.setenv("plotly_api_key" = plotly.key)
+
 
 # This block will try to find out the terrorist attact per year
 
@@ -14,3 +18,11 @@ attack.per.year <- DATA %>%
 				   											ifelse(iyear <= 2011, "ISVG", 'START'))))
 
 write.csv(attack.per.year, './data/attack.per.year.csv')
+
+# This block will show graph of attacks each year
+pdf(paste('./data/', 'attack_per_year.pdf', sep = ''))
+ggplot(data = attack.per.year) +
+ 	   geom_point(mapping = aes(x = iyear, y = times, 
+		 						color = `Collected Institution`)) +
+	   labs(title = "Attack time in time",
+			x = 'Year', y = "Times")
