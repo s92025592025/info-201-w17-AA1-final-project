27f045e3da8c719a3b471b8c6761c7681086357e
Daniel Wang <s92025592025@gmail.com>
2017-03-02 22:00:47 -0800

    starter file to analyze data

---
+++ analysis.R
@@ -0,0 +1,11 @@
+# Final Project
+# Info 201 AA1
+# 2017/03/02
+# This is a file contains functions to generate plots to show on a pages
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(maps)
+library(stringr)
+library(plotly)
