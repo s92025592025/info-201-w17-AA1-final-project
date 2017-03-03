589c5c44aa3a18185cc2362250ee606134036db2
Daniel Wang <s92025592025@gmail.com>
2017-03-03 10:19:48 -0800

    Add testing files to ignore

--- analysis.R
+++ analysis.R
@@ -12,11 +12,13 @@ library(plotly)
 
 DATA <- read.csv('./data/globalterrorismdb_0616dist.csv', stringsAsFactors = FALSE)
 ISO3.CONVERT <- read.csv('./data/country_data.csv', stringsAsFactors = FALSE)
+DATA.w.ISO3 <- left_join(DATA, ISO3.CONVERT)
 
 # pre: should pass as ISO3(current) string(ALL CAPS) to country, a vector of a starting year and
 #	   ending year(numbers) to year.range, and a list of filters to selected.
 #	   Format for filter: ['col.name': 'attribute to filter']
 #	   Example for filter: [attacktype1_txt: 'Assassination', targtype1_txt: 'Private Citizens & Property']
+#
 # post: Will return a list of plotly pie charts indicating Attacks Type, Targets,
 #		and Used Weapons
 Attack.Pies <- function(country, year.range, selected){}
