969f5dd7becfe3124429a3c616077f6754190531
Daniel Wang <s92025592025@gmail.com>
2017-03-06 22:04:11 -0800

    new function to filter data for pie chart at once

--- analysis.R
+++ analysis.R
@@ -23,21 +23,7 @@ DATA.w.ISO3 <- left_join(DATA, ISO3.CONVERT)
 # post: Will return a list of plotly pie charts indicating Attacks Type, Targets,
 #		and Used Weapons
 Attack.Info.Pies <- function(country.iso3, year.range, selected){
-	# Filters out the data within the year.range
-	filtered <- DATA.w.ISO3 %>%
-				filter(iyear >= year.range[1], iyear <= year.range[2])
-
-	# filter out the selected country if needed
-	if(country.iso3 != 'WORLD'){
-		filtered <- filtered %>%
-					filter(New.ISO3 == country.iso3)
-	}
-
-	# filter out the selected data
-	for(key in names(selected)){
-		filtered <- filtered %>%
-					filter_(paste0(key, '=="' ,selected[key], '"'))
-	}
+	filtered <- Pie.Data.Filter(country.iso3, year.range, selected)
 
 	return(list(type = Attack.Type.Pie(filtered), targets = Attack.Target.Pie(filtered),
 				weap = Attack.Weap.Pie(filtered)))
@@ -78,7 +64,6 @@ Attack.Target.Pie <- function(data){
 		   layout(title = "Attack Targets",
 		 		  xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
          		  yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)))
-
 }
 
 
@@ -101,7 +86,42 @@ Attack.Weap.Pie <- function(data){
          		  yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)))
 }
 
+# pre: should pass as ISO3(current) string(ALL CAPS) or 'WORLD' to country.iso3, a vector of a starting year
+#	   and ending year(numbers) to year.range, and a list of filters to selected.
+#	   Format for filter: ['col.name'='attribute to filter']
+#	   Example for filter: [attacktype1_txt='Assassination', targtype1_txt='Private Citizens & Property']
+#
+# post: will return a list of information that are not duplicated in Attack type, targets and weapon
+Attack.Info.List <- function(country.iso3, year.range, selected){
+	filtered <- Pie.Data.Filter(country.iso3, year.range, selected)
+	
+}
 
+# pre: should pass as ISO3(current) string(ALL CAPS) or 'WORLD' to country.iso3, a vector of a starting year
+#	   and ending year(numbers) to year.range, and a list of filters to selected.
+#	   Format for filter: ['col.name'='attribute to filter']
+#	   Example for filter: [attacktype1_txt='Assassination', targtype1_txt='Private Citizens & Property']
+#
+# post: will return a data frame that was filtered by give filter
+Pie.Data.Filter <- function(country.iso3, year.range, selected){
+	# Filters out the data within the year.range
+	filtered <- DATA.w.ISO3 %>%
+				filter(iyear >= year.range[1], iyear <= year.range[2])
+
+	# filter out the selected country if needed
+	if(country.iso3 != 'WORLD'){
+		filtered <- filtered %>%
+					filter(New.ISO3 == country.iso3)
+	}
+
+	# filter out the selected data
+	for(key in names(selected)){
+		filtered <- filtered %>%
+					filter_(paste0(key, '=="' ,selected[key], '"'))
+	}
+
+	return(filtered)
+}
 
 Global.Terrorism.Attacks <- function(year) {
 	world <- map_data("world")
