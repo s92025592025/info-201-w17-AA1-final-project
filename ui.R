135f6de3d8cdebd85d029aed9e9f7a8099840187
Daniel Wang <s92025592025@gmail.com>
2017-03-09 00:28:15 -0800

    layout code

--- ui.R
+++ ui.R
@@ -11,27 +11,29 @@ ui <- tagList(
   ),
   navbarPage( theme  =  shinythemes::shinytheme("united"), "Global Terrorism Database",
                   tabPanel("Introduction"),
-                             tabPanel("Data Visualization",
-                                      fluidPage(
-                                        fluidRow(
-                                          column(width =  4, selectInput("country", "country", names(All.Country.List()))),
-                                          column(width = 4, sliderInput("year", "Year range:", min = 1970, max = 2015, value = c(1970,2015))),
-                                          column(width = 8, plotlyOutput("graph"))
-                                        ),                                        
-                                        tags$hr(style="border-color: black;"),
+                  tabPanel("Data Visualization",
+                            fluidPage(
+                              fluidRow(
+                                column(width =  4, selectInput("country", "country", names(All.Country.List()))),
+                                column(width = 4, sliderInput("year", "Year range:", min = 1970, max = 2015, value = c(1970,2015))),
+                                column(width = 8, plotlyOutput("graph"))
+                              ),                                        
+                              tags$hr(style="border-color: black;"),
 
-                                        fluidRow(
-                                          column(width = 4, selectInput("type.select", 'Select Attack Type', choices = c()),
-                                                            plotlyOutput("type.pie")),
-                                          column(width = 4, selectInput("target.select",'Select Target Type', choices = c()),
-                                                            plotlyOutput("target.pie")),
-                                          column(width = 4, selectInput("weap.select", 'Select Weapon Type',choices = c()),
-                                                            plotlyOutput("weap.pie"))
-                                        ),
-                                        verbatimTextOutput("click"),
-                                        tags$hr(style="border-color: black;"),
-                                          div(plotlyOutput("plot")) 
-                                      )
-)))
+                              fluidRow(
+                                column(width = 4, selectInput("type.select", 'Select Attack Type', choices = c()),
+                                                  plotlyOutput("type.pie")),
+                                column(width = 4, selectInput("target.select",'Select Target Type', choices = c()),
+                                                  plotlyOutput("target.pie")),
+                                column(width = 4, selectInput("weap.select", 'Select Weapon Type',choices = c()),
+                                                  plotlyOutput("weap.pie"))
+                              ),
+                              verbatimTextOutput("click"),
+                              tags$hr(style="border-color: black;"),
+                                div(plotlyOutput("plot")) 
+                            )
+                  )
+            )
+)
 
 shinyUI(ui)
