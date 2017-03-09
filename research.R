2e00384d875c72af581baf93195e87eca6550476
Daniel Wang <s92025592025@gmail.com>
2017-03-08 21:31:36 -0800

    Add csv for every five years

--- research.R
+++ research.R
@@ -106,3 +106,14 @@ kill <- DATA %>%
 		filter(iyear >= 1990)
 
 write.table(kill, file = "./data/trimmed.csv", row.name = FALSE, sep =',')
+
+# data for every five year
+
+every.five <- DATA %>%
+			  select(iyear, attacktype1_txt, targtype1_txt, weaptype1_txt, country_txt, success, suicide) %>%
+			  mutate(year.group = (iyear - 1970) / 5) %>%
+			  group_by(country_txt, year.group, success, suicide, attacktype1_txt, targtype1_txt, weaptype1_txt) %>%
+			  summarise(times = n()) %>%
+			  mutate(iyear = 1970 + year.group * 5)
+
+write.table(every.five, file = "./data/everyfive.csv", row.name = FALSE, sep =',')
